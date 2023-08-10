f1e0e48ad66dcc067057b71ee0ac6113c2e6d675
john <Johnchristuraj>
2023-08-10 09:58:20 +0530

    First commit

---
+++ app_server/controller/locations.js
@@ -0,0 +1,13 @@
+module.exports.homeList= function(req,res){
+    res.render('location-list',{title:'Home'});
+};
+
+module.exports.locationInfo= function(req,res){
+    res.render('location-info',{title:'location '});
+};
+module.exports.locationInfo2= function(req,res){
+    res.render('location-info2',{title:'location '});
+};
+module.exports.addReview= function(req,res){
+    res.render('location-review-form',{title:'review'});
+};
